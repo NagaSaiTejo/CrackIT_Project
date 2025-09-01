769afa58bee02a25ec433b2c4aaf0d19d23a902a
NagaSaiTejo <nagasaitejo@gmail.com>
2025-09-01 12:04:51 +0530

    Initial Push

---
+++ android/app/src/main/kotlin/com/example/crackit/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.crackit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/global_favorites.dart
@@ -0,0 +1,6 @@
+import 'package:crackit/models/question_model.dart';
+
+List<QuestionModell> globalAptitudeFavorites = [];
+List<QuestionModel> globalDsaFavorites = [];
+List<ManagerialQuestionModel> globalManagerialFavorites = [];
+List<HrQuestionModel> globalHrFavorites = [];
